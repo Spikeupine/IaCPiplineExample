3c781f89b0f82feaaff67b4e02f6b19417c9f191
Trygve <sollund421@gmail.com>
2024-10-23 15:49:13 +0200

    fixed backend not working

--- providers.tf
+++ providers.tf
@@ -2,17 +2,29 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.73.0"
-    }
+      version = "4.0.1"
+      }
   }
+  
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-backend-tim"
-    storage_account_name = "sabetfstimcfw8rawacd"
+    resource_group_name  = "rg-terraform-backend-trygve"
+    storage_account_name = "tfstatev3fkw"
     container_name       = "tfstate"
-    key                  = "web.terraform.tfstate"
+    key                  = "webexample.terraform.tfstate"
   }
 }
 
 provider "azurerm" {
+  subscription_id = var.subscription_id
   features {}
+}
+
+data "terraform_remote_state" "foo" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "rg-terraform-backend-trygve"
+    storage_account_name = "tfstatev3fkw"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -26,3 +26,8 @@ variable "index_document" {
   description = "Name of the index document"
   default     = "index.html"
 }
+
+variable "subscription_id" {
+  type = string
+  sensitive = true
+}
